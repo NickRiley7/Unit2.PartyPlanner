be998387faa7a8a8111d4d426a31695d6c1687c7
NickRiley7 <nick_riley@LAPTOP-J8V35VGC>
2023-10-31 20:26:00 -0400

    corrected code again

--- party planner/index.js
+++ party planner/index.js
@@ -1,5 +1,4 @@
 const COHORT = "2309-FTB-ET-WEB-FT";
-<<<<<<< HEAD
 const API = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/" + COHORT;
 
 const state = {
@@ -70,54 +69,3 @@ function render() {
 }
 
 getEvents();
-=======
-const API_URL = `https:fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/resources`;
-
-const state = {
-    resources: [],
-  };
-
-  const resourceList = document.querySelector("#resources");
-
-const addResourceForm = document.querySelector("#addResource");
-addResourceForm.addEventListener("submit", addResource);
-
-async function render() {
-    await getRecipes();
-    renderRecipes();
-    console.log('state', state)
-  }
-  render();
-
-  async function getRecipes() {
-    try {
-      const reponse = await fetch(API_URL);
-      const json = await response.json();
-      state.recipes = json.data;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  function renderRecipes() {
-    if (!state.recipes.length) {
-      recipeList.innerHTML = "<li>No recipes.</li>";
-      return;
-    }
-  
-    const recipeCards = state.recipe.map((recipe) => {
-      const li = document.createElement("li");
-      li.innerHTML = `
-        <h2>${recipe.name}</h2>
-        <img src="${recipe.imageUrl}" alt="${recipe.name}" />
-        <p>${recipe.description}</p>
-      `;
-      return li;
-    });
-  
-    recipeList.replaceChildren(...recipeCards);
-  }
-
-  
-  
->>>>>>> a15c2847d87585fe4bb57c26521bdc66ffa5c3c5
